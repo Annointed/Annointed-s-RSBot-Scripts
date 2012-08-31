83b6948db17734e58f448dedd288abfd7f4e546c
Jonathan Chak <babadu1234@inbox.lv>
2012-08-31 13:05:37 -0400

    Added EnterLobby class

---
+++ org/annointed/scripts/anfistofguthix/EnterLobby.java
@@ -0,0 +1,31 @@
+package org.annointed.scripts.anfistofguthix;
+
+import org.annointed.scripts.anfistofguthix.constants.Constants;
+import org.powerbot.concurrent.strategy.Strategy;
+import org.powerbot.game.api.methods.interactive.NPCs;
+import org.powerbot.game.api.methods.node.SceneEntities;
+import org.powerbot.game.api.util.Time;
+import org.powerbot.game.api.wrappers.interactive.NPC;
+import org.powerbot.game.api.wrappers.node.SceneObject;
+
+public class EnterLobby extends Strategy implements Runnable {
+
+	@Override
+	public void run() {
+		SceneObject passageWay = SceneEntities
+				.getNearest(Constants.ENTER_PASSAGEWAY_ID);
+		if (passageWay.isOnScreen()) {
+			passageWay.interact("Go-through");
+			Time.sleep(3000, 3500);
+		}
+	}
+
+	@Override
+	public boolean validate() {
+		NPC fiara = NPCs.getNearest(Constants.FIARA_ID);
+		SceneObject passageWay = SceneEntities
+				.getNearest(Constants.ENTER_PASSAGEWAY_ID);
+		return fiara != null && passageWay != null;
+	}
+
+}
