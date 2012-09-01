26fb0213e24b4200334a67545ed7673c7207760d
Jonathan Chak <babadu1234@inbox.lv>
2012-09-01 12:36:54 -0400

    Returns if selected

--- org/annointed/scripts/anfistofguthix/wrappers/Methods.java
+++ org/annointed/scripts/anfistofguthix/wrappers/Methods.java
@@ -5,22 +5,19 @@ import java.awt.Point;
 import org.powerbot.game.api.methods.input.Mouse;
 import org.powerbot.game.api.methods.node.Menu;
 import org.powerbot.game.api.util.Random;
-import org.powerbot.game.api.util.Time;
 import org.powerbot.game.api.wrappers.Entity;
 
 public class Methods {
 
-	public static void interact(final Entity entity, final String action) {
+	public static boolean interact(final Entity entity, final String action) {
 		int random = Random.nextInt(1, 3);
 		Point entityPoint = new Point(entity.getCentralPoint().x + random,
 				entity.getCentralPoint().y + random);
 		Mouse.hop(entityPoint.x, entityPoint.y);
-		if (Menu.contains(action)) {
-			Menu.select(action);
-			Time.sleep(1000, 1500);
-		} else {
-			return;
-		}
+		if (Menu.contains(action))
+			return Menu.select(action);
+		else
+			return false;
 	}
 
 }
